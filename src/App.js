def77a9fe5982c786983cf09455116c6bfdbd714
Waru Clan <waruprotocol@gmail.com>
2023-05-21 16:52:41 +0200

    202305211652

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+//import "./styles/Home.css";
+import {Box, Card, Container, Flex, Heading, Image, SimpleGrid, Spinner,Text } from "@chakra-ui/react";
+import {ConnectWallet, MediaRenderer, useContract, useAddress, useOwnedNFTs } from "@thirdweb-dev/react";
+import waruClan from "./components/waru.png";
 
 export default function Home() {
+  const myAddress=useAddress();
+  const {contract:wubiContract}=useContract("0xde16ea1d873e3dd149c488a4c14b8116244e9b51");
+  const {data:myOwnedNFTs, isLoading:loadingOwnedNFTs}=useOwnedNFTs(wubiContract,myAddress);
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
-        </p>
-
-        <div className="connect">
-          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} />
-        </div>
-
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
+    <Container maxW={"1200px"} py={5}>
+      <Flex justifyContent={"space-between"} alignItems={"center"}>
+        <Text fontWeight={"bold"}>Waru Clan</Text>
+        <ConnectWallet/>
+      </Flex>
+      <br/>
+      <Flex>
+        <Image src={waruClan} borderRadius={15}/>
+      </Flex>
+      {loadingOwnedNFTs ? (
+        <Flex h={"90vh"} direction={"column"} justifyContent={"center"} align={"center"}>
+          <Spinner/>
+        </Flex>
+      ) : (
+        <Container maxW={"1200)x"} mt={10}>
+        <Box>
+            <Text fontWeight={"bold"}>My NFTs :</Text>
+            <SimpleGrid columns={4} spacing={5} my={5}>
+                {myOwnedNFTs?.map((wubiNFT)=>(
+                    <Card overflow={"hidden"} p={2}>
+                        <Flex justifyContent={"space-between"} alignItems={"center"}>
+                            <Text ml={4} fontWeight={"bold"}>{wubiNFT.metadata.name}</Text>
+                        </Flex>
+                        <MediaRenderer src={wubiNFT.metadata.image}/>
+                    </Card>
+                ))}
+            </SimpleGrid>
+        </Box>
+    </Container>
+      )
+      }
+    </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+//Desactivation du ReportWebVitalis
+//import reportWebVitals from "./reportWebVitals";
+
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import ProfilePage from "./profile/address";
+
+//Fichier CSS livré en standard mais désactivé au profit de Chakra
+//import "./styles/globals.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
+    <ThirdwebProvider activeChain="polygon">
+      <ChakraProvider>
+        <App/>
+        {/* <ProfilePage/> */}
+      </ChakraProvider>
     </ThirdwebProvider>
   </React.StrictMode>
 );
@@ -23,4 +28,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
